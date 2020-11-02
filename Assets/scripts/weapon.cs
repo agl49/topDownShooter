80d63e0320dc211c5b599e0404e6da919e4932d8
agl49 <agl49@txstate.edu>
2020-11-01 18:49:22 -0600

    refactored and started weapon pickup

---
+++ Assets/scripts/weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class Weapon : ScriptableObject
+{
+    public GameObject bulletPrefab;
+    public float fireRate = 1f;
+    public int damage = 1;
+
+    public void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, 
+                                        GameObject.Find("FirePoint").transform.position, 
+                                        Quaternion.identity);
+                                        
+    }
+}
