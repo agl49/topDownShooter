6a33d3a1487919db9bdb4f91810f707fe6190fdf
agl49 <agl49@txstate.edu>
2020-11-04 09:31:24 -0600

    weapon works sorta

--- Assets/scripts/Shooting.cs
+++ Assets/scripts/Shooting.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public Transform firePoint;
-    public GameObject bulletPrefab;
-
-    public float bulletForce = 20f;
-
+    public Weapon currentWeapon;
+    
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            currentWeapon.shoot();
         }
     }
 
     void Shoot()
     {
+        
+     
+        /*
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+   */
     }
+
 }
