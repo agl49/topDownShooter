4374380b9388fe44165a5c9a4415750a5770816b
Jeremy Gonzales <gonzalesjeremy39@gmail.com>
2020-11-04 17:57:13 -0600

    updated enemy
    Updated enemy that will only follow player when inside his set zone and only fire when in firing range.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-   public float moveSpeed = 5f;
+   public float moveSpeed = 2f;
+   public float lineOfSite;
+   public float shootingRange;
+   public float fireRate = 1f;
+   private float nextFireTime;
+   public GameObject bullet;
+   public GameObject bullentParent;
+   private Transform player;
 
    public Rigidbody2D rb;
 
    Vector2 movement;
+
+   void Start()
+   {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+
+   }
     // Update is called once per frame
     void Update()
     {
+        float distanceFromPlayer = Vector2.Distance(player.position,transform.position);
+        if(distanceFromPlayer<lineOfSite && distanceFromPlayer>shootingRange){
+            transform.position= Vector2.MoveTowards(this.transform.position,player.position, moveSpeed*Time.fixedDeltaTime);
+        }
+        else if(distanceFromPlayer <= shootingRange && nextFireTime < Time.time)
+        {
+            Instantiate(bullet, bullentParent.transform.position,Quaternion.identity);
+            nextFireTime = Time.time + fireRate;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate(){
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
+    private void OnDrawGizmosSelected(){
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
     }
+
+    // void FixedUpdate(){
+    //     rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    // }
 }
---
+++ Assets/ShootTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTarget : MonoBehaviour
+{
+    GameObject target;
+    //  public Transform firePoint;
+    public float speed;
+    Rigidbody2D bulletRB;
+    // public float bulletForce = 20f;
+    // public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRB = GetComponent< Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDirection = (target.transform.position - transform.position).normalized * speed;
+        bulletRB.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(this.gameObject,2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //  void Shoot()
+    // {
+    //     GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    //     Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+    //     rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    // }
+}
