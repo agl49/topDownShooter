6ab3c812a00ddd0d8d320ca6e39b84d549bf599f
agl49 <agl49@txstate.edu>
2020-11-08 18:16:35 -0600

    fixing duplicate shooting methods

--- Assets/OGShooting.cs
+++ Assets/OGShooting.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class OGShooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
