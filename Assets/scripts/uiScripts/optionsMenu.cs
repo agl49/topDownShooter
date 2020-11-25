ca1c1637238f337a95db71962d4137ef926bcd9f
agl49 <agl49@txstate.edu>
2020-11-24 21:01:13 -0600

    option menu done

---
+++ Assets/scripts/uiScripts/optionsMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class optionsMenu : MonoBehaviour
+{
+    private Resolution[] resolutions;
+
+    public TMP_Dropdown resolutionDropDown;
+
+    public AudioMixer audioMixer;
+
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+               resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, 
+                             Screen.fullScreen); 
+    }
+
+    public void setVolume(float vol)
+    {
+        audioMixer.SetFloat("masterVolume", vol);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
