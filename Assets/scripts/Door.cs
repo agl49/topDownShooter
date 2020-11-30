16d316f417e143c147b1347102af5eb882099b62
j_d537 <j_d537@txstate.edu>
2020-11-29 21:22:18 -0600

    addition to level 3

---
+++ Assets/scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // this function will check for the exit door
+    public void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Exit")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
